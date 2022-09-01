b19ab132db00838fa0cab1522b55eb7f3baaf718
levideutsch <levideutsch@live.com>
2022-09-01 13:48:46 -0400

    commit tree 9e8d59ebe820811437f11848a25d6503c808568c

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 25
+let num4 = 7
+let mod = num3 % num4
+
+let max = Math.max(1, 3, 6, 7, 15, 19, 20)
+
+
